027c7d7e20eaf413472cb7e2fabac8e53b39f7a7
nguyenhung1181 <nguyenmanhhung110801@gmail.com>
2021-12-22 15:06:18 +0700

    Update

--- WebApp1/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ WebApp1/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -19,6 +19,7 @@ namespace WebApp1.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {
+
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
